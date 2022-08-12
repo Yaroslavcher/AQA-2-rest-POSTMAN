3fce95c7ee33669ffb09be5cf05e92ba3db1d8ce
Ярослав Черников <yaroslavcher@gmail.com>
2022-08-12 23:22:12 +0300

    first commit

---
+++ src/test/java/ru/netology/postman/POSTEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class POSTEchoTest {
+
+    @Test
+    void shouldResponseEcho()
+        given()
+            .baseUri("https://postman-echo.com")
+            .body("the value")
+        .when()
+            .post("/post")
+        .then()
+            .statusCode(200)
+            .body("data", equalTo("the value"));
+
+}
