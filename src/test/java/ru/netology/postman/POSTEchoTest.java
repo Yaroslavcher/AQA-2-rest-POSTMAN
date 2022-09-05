b813d6c81a413cbb7928fe1a09b7bf385763619e
Yaroslav <yaroslavcher@gmail.com>
2022-09-05 20:16:34 +0300

    error using json-schema

--- src/test/java/ru/netology/postman/POSTEchoTest.java
+++ src/test/java/ru/netology/postman/POSTEchoTest.java
@@ -3,11 +3,25 @@ package ru.netology.postman;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 
 public class POSTEchoTest {
     @Test
     void shouldResponseEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("the value")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("the value"))
+                .body(matchesJsonSchemaInClasspath("echo.schema.json"));
+    }
+
+    @Test
+    void shouldResponseInvalidEcho() {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("the value")
